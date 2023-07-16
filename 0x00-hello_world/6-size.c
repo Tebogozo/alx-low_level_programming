20518af38a04003dd7e8491c3759b1f5210d3f93
tebogo <tebomphuthi56406@gmail.com>
2023-07-16 04:34:34 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,15 +11,11 @@ int b;
 long int c;
 long int d;
 float f;
-double g;
-long double h;
 
 printf("Size of a char: %lu byte(s)\n", sizeof(a));
 printf("Size of an int: %lu byte(s)\n", sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
 printf("Size of a float: %lu byte(s)\n", sizeof(f));
-printf("Size of a double: %lu byte(s)\n", sizeof(g));
-printf("Size of a long double: %lu byte(s)\n", sizeof(h));
 return (0);
 }
