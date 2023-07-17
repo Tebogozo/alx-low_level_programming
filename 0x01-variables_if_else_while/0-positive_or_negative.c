f17f46a38e786b9533b61dfa28678d24293df75d
tebogo <tebomphuthi56406@gmail.com>
2023-07-17 11:19:17 -0700

    pnN

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,29 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: 0 if false.  positive, negative or zero
+*/
 int main(void)
 {
-    srand(time(NULL));
-    int n = rand() % RAND_MAX;
+srand(time(NULL));
+int n = rand() % RAND_MAX;
 
-    std::cout << "The number " << n;
+std::cout << "The number " << n;
 
-    if (n > 0)
-    {
-        std::cout << " is positive";
-    }
-    else if (n == 0)
-    {
-        std::cout << " is zero";
-    }
-    else
-    {
-        std::cout << " is negative";
-    }
-
-    std::cout << std::endl;
-    return 0;
+if (n > 0)
+{
+std::cout << " is positive";
+}
+else if (n == 0)
+{
+std::cout << " is zero";
+}
+else
+{
+std::cout << " is negative";
 }
 
+std::cout << std::endl;
+return (0);
+}
