845f685da73f4a773c1a00f5c1842147e5de4230
tebogo <tebomphuthi56406@gmail.com>
2023-07-23 09:29:47 -0700

    last digital

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,6 +7,8 @@
 * Return: Always 0
 */
 void test_case_1(void);
+void test_case_2(void);
+void test_case_3(void);
 
 int main(void)
 {
@@ -15,10 +17,10 @@ srand(time(0));
 
 test_case_1();
 
-return 0;
+return (0);
 }
 
-void test_case_1(void)
+void test_case_1(void) /* generate and return to main */
 {
 int numbers[] = {98, -98, 980};
 
@@ -42,4 +44,59 @@ printf(" and is less than 6 and not 0");
 }
 
 printf("\n");
+
+}
+
+void test_case_2(void) /* generate and return to main */
+{
+int numbers[] = {98, -98, 980};
+
+int randomIndex = rand() % 4;
+int n = numbers[randomIndex];
+int lastDigit = n % 10;
+
+printf("Last digit of %d is %d", n, lastDigit);
+
+if (lastDigit > 5)
+{
+printf(" and is greater than 5");
+}
+else if (lastDigit == 0)
+{
+printf(" and is 0");
+}
+else
+{
+printf(" and is less than 6 and not 0");
+}
+
+printf("\n");
+
+}
+
+void test_case_3(void) /* generate and return to main */
+{
+int numbers[] = {98, -98, 980};
+
+int randomIndex = rand() % 4;
+int n = numbers[randomIndex];
+int lastDigit = n % 10;
+
+printf("Last digit of %d is %d", n, lastDigit);
+
+if (lastDigit > 5)
+{
+printf(" and is greater than 5");
+}
+else if (lastDigit == 0)
+{
+printf(" and is 0");
+}
+else
+{
+printf(" and is less than 6 and not 0");
+}
+
+printf("\n");
+
 }
