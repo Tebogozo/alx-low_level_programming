da7df4ddf92bff5441fd853be22907122de6359a
tebogo <tebomphuthi56406@gmail.com>
2023-07-23 12:16:05 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,97 +6,30 @@
 *
 * Return: Always 0
 */
-void test_case_1(void);
-void test_case_2(void);
-void test_case_3(void);
 
 int main(void)
 {
-/* Seed the random number generator */
-srand(time(0));
 
-test_case_1();
+int n;
+int last_digit;
+
+srand(time(0));
+n = rand()- RAND_MAX / 2;
+last_digit = n% 10;
+
+if (last_digit > 5)
+{
+printf("Last digit of %d is %d and greater than 5\n", n, last_digit);
+}
+else if (last_digit == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
+}
+else
+{
+printf("Last digit of %d is %d and is less than 6 and not 0", n, last_digit);
+}
+printf("\n");
 
 return (0);
-}
-/* generate and return to main*/
-void test_case_1(void) /* generate and return to main*/
-{
-int numbers[] = {98, -98, 980};
-
-int randomIndex = rand() % 3;
-int n = numbers[randomIndex];
-int lastDigit = n % 10;
-
-printf("Last digit of %d is %d", n, lastDigit);
-
-if (lastDigit > 5)
-{
-printf(" and is greater than 5");
-}
-else if (lastDigit == 0)
-{
-printf(" and is 0");
-}
-else
-{
-printf(" and is less than 6 and not 0");
-}
-
-printf("\n");
-
-}
-/* generate and return to main*/
-void test_case_2(void)/* generate and return to main*/
-{
-int numbers[] = {98, -98, 980};
-
-int randomIndex = rand() % 3;
-int n = numbers[randomIndex];
-int lastDigit = n % 10;
-
-printf("Last digit of %d is %d", n, lastDigit);
-
-if (lastDigit > 5)
-{
-printf(" and is greater than 5");
-}
-else if (lastDigit == 0)
-{
-printf(" and is 0");
-}
-else
-{
-printf(" and is less than 6 and not 0");
-}
-
-printf("\n");
-
-}
-/* generate and return to main*/
-void test_case_3(void) /* generate and return to main*/
-{
-int numbers[] = {98, -98, 980};
-
-int randomIndex = rand() % 3;
-int n = numbers[randomIndex];
-int lastDigit = n % 10;
-
-printf("Last digit of %d is %d", n, lastDigit);
-
-if (lastDigit > 5)
-{
-printf(" and is greater than 5");
-}
-else if (lastDigit == 0)
-{
-printf(" and is 0");
-}
-else
-{
-printf(" and is less than 6 and not 0");
-}
-
-printf("\n");
-
 }
