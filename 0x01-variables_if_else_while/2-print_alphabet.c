5018280cc2d5f4301faf3a8a84e39ff71ed24d28
tebogo <tebomphuthi56406@gmail.com>
2023-07-24 12:40:11 -0700

    alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-
 /**
 * main - entry point
-*"Print alphabets lowercase'
-*return always 0
+* "Print alphabets lowercase'
+* Return: always 0 on successful execution, non-zero otherwise
 */
-
-int main(void)
+int main(void) /* main will be returning result*/
 {
 int n = 97;
 
-while(n <= 122)
+while (n <= 122)
+
 {
 putchar(n);
 n++;
