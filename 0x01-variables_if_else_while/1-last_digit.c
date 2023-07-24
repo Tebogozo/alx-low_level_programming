502e1d321e1186f902f86122e2e4f9a1f17c4989
tebogo <tebomphuthi56406@gmail.com>
2023-07-24 12:28:58 -0700

    alpha

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ last_digit = n% 10;
 
 if (last_digit > 5)
 {
-printf("Last digit of %d is %d and greater than 5\n", n, last_digit);
+printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 }
 else if (last_digit == 0)
 {
@@ -27,9 +27,7 @@ printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
 else
 {
-printf("Last digit of %d is %d and is less than 6 and not 0", n, last_digit);
+printf("Last digit of %d is %d and less than 6 and not 0\n", n, last_digit);
 }
-printf("\n");
-
 return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*"Print alphabets lowercase'
+*return always 0
+*/
+
+int main(void)
+{
+int n = 97;
+
+while(n <= 122)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
